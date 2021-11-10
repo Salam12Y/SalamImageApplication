cb809050a325ad4d966268e1e499bb7b92868e40
doniataskman <doniahammud@gmail.com>
2021-11-10 08:55:26 +0200

    54ytryh

---
+++ app/src/main/java/yassen/salam/salamimageapplication/addAlbumActivity.java
@@ -0,0 +1,13 @@
+package yassen.salam.salamimageapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addAlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_album);
+    }
+}
